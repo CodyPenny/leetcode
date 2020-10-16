fcbac12d173395d35dbaa0269ed291c963e8506b
evelyn <evelynrhee103@gmail.com>
2020-10-16 00:25:14 -0700

    complete

--- romanToInt.js
+++ romanToInt.js
@@ -18,5 +18,33 @@ var romanToInt = function (s) {
     D: 500,
     M: 1000
   };
-  
+  let num = 0;
+  let first = 0;
+  let second = 0;
+
+  while (first < s.length) {
+    second = first + 1;
+    if (s[second]) {
+      if (roman[s[second]] > roman[s[first]]) {
+        num += roman[s[second]] - roman[s[first]];
+        first += 2;
+      } else if (roman[s[second]] < roman[s[first]]) {
+        num += roman[s[first]];
+        first++;
+      } else {
+        num += roman[s[first]];
+        first++;
+      }
+    } else {
+      num += roman[s[first]];
+      first++;
+    }
+  }
+
+  return num;
 };
+/*
+Success
+Details 
+Runtime: 168 ms, faster than 60.50% of JavaScript online submissions for Roman to Integer.
+Memory Usage: 44.8 MB, less than 5.32% of JavaScript online submissions for Roman to Integer.*/
